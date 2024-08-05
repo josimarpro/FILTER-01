80e94027f0fe0becd0eecd036099aeb9f23880a7
josimar ferreira <josima-tamires@hotmail.com>
2024-08-05 19:10:32 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let pessoas =[
+    {nome: 'Roberto', idade:33 },
+    {nome: 'Josimar', idade:33 },
+    {nome: 'Rafael', idade:27 }
+]
+ 
+let pessoasComIdadesDE33Anos = []
+for(let i =0; i <pessoas.length; i++){
+    if(pessoas[i].idade === 33){
+        pessoasComIdadesDE33Anos.push(pessoas[i])
+    }
+}
+console.log(pessoascoidadede33anos)
